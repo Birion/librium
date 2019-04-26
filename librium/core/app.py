76c3a2dd34bc62fb184f3e29b6c9f8872f4e8491
Ondřej Vágner <ondrej.vagner@gmail.com>
2019-04-26 09:28:05 +0200

    kivy start

--- librium/core/app.py
+++ librium/core/app.py
@@ -1,15 +1,18 @@
 import kivy
 from kivy.uix.widget import Widget
-from kivy.properties import StringProperty
+from kivy.properties import ObjectProperty
 from kivy.app import App
+
+from librium.database import *
 
 kivy.require("1.10.1")
 
 
-class LibriumBook(Widget):
-    title = StringProperty("None")
+# class LibriumBook(Widget):
+#     book = ObjectProperty(None)
 
 
 class LibriumApp(App):
-    def build(self):
-        return LibriumBook()
+    pass
+    # def build(self):
+    #     return LibriumBook()
