6bbff59f6fd63c707b2cdd94a64b4a641c393dd7
Ondřej Vágner <ondrej.vagner@gmail.com>
2024-05-28 19:21:11 +0200

    Add environment variable for cover folder path
    The code now uses an environment variable to set the path of the covers folder. If no environment variable is found, the default path is "covers". The dotenv package is used to load the variable from a .env file.

--- librium/views/covers.py
+++ librium/views/covers.py
@@ -1,10 +1,16 @@
+import os
+from pathlib import Path
+
+from dotenv import find_dotenv, load_dotenv
 from flask import Blueprint
 
-from pathlib import Path
+load_dotenv(find_dotenv())
+
+covers_folder = os.getenv("COVERS_FOLDER") if os.getenv("COVERS_FOLDER") else "covers"
 
 bp = Blueprint(
     "covers",
     __name__,
-    static_folder=Path("covers").resolve(),
+    static_folder=Path(covers_folder).resolve(),
     static_url_path="/covers",
 )
